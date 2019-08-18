f9944ebb2846aacdb43a265c32dc3faa62d60db3
qingzhu1224 <yanqingzhu@hualala.com>
2019-08-18 16:54:12 +0800

    添加docs

--- examples/main.js
+++ examples/main.js
@@ -2,24 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import VVUI from '../packages/index'
 import '../packages/theme/lib/index.css' 
-import VueRouter from 'vue-router'
 import demoBlock from './components/demo-block.vue'
+import router from './router'
 Vue.use(VVUI)
 
-Vue.use(VueRouter)
 
 Vue.component('demo-block', demoBlock)
 
 Vue.config.productionTip = false
 
-let router = new VueRouter({
-  routes: [
-      //一个个对象
-      { path: '/home',  component: r => require.ensure([], () => r(require('../doc/test.md'))) }
-  ]
-});
 
 new Vue({
-  router: router, //可以简写router
+  router, //可以简写router
   render: h => h(App)
 }).$mount('#app')
